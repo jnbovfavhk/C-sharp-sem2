2b362cdcde5bc36a6fb2ffe9516e39359ce43ab4
belonozhkoin <belonozhkoin@W12-229-10>
2025-02-28 11:20:45 +0400

    сгу комп может запускать

--- prac_18/Program.cs
+++ prac_18/Program.cs
@@ -13,10 +13,11 @@ namespace ConsoleApp1
         {
             //Example.Main1();
             //return;
-            Console.WriteLine("Задание 1\r\n1. Создать абстрактный класс Figure с методами вычисления площади и периметра, " +
+            Console.WriteLine("Задание 18.1\r\n1. Создать абстрактный класс Figure с методами вычисления площади и периметра, " +
                 "а также\r\nметодом, выводящим информацию о фигуре на экран.\r\n2. Создать производные классы: Rectangle (прямоугольник), " +
                 "Circle (круг), Triangle\r\n(треугольник) со своими методами вычисления площади и периметра.\r\n3. " +
-                "Создать массив n фигур и вывести полную информацию о фигурах на экран.");
+                "Создать массив n фигур и вывести полную информацию о фигурах на экран." +
+                "Задание 19. В классе Figure создать метод CompareTo, чтобы фигуры можно было отсортировать по площадям");
 
             List<Figure> figures = Deserialize();
 
@@ -44,9 +45,10 @@ namespace ConsoleApp1
 
         public static void Serialize(List<Figure> figures)
         {
-            // gдготовка строки json для сериализации с конввертацией из FigureConverter
+            // подготовка строки json для сериализации с конввертацией из FigureConverter
             var jsonString = JsonConvert.SerializeObject(figures, Formatting.Indented, new FigureConverter());
-            using (StreamWriter f = new StreamWriter("C:\\Users\\ilyab\\source\\repos\\ConsoleApp1\\prac_18\\data.json"))
+            using (StreamWriter f = new StreamWriter("C:\\Users\\belonozhkoin\\Source\\Repos\\C-sharp-sem2\\prac_18\\data.json"))
+            //using (StreamWriter f = new StreamWriter("C:\\Users\\ilyab\\source\\repos\\ConsoleApp1\\prac_18\\data.json"))
             {
                 f.Write(jsonString);
             }
@@ -60,15 +62,18 @@ namespace ConsoleApp1
             List<Figure> figures = new List<Figure>();
 
             // Если файла нет, создать
-            if (!File.Exists("C:\\Users\\ilyab\\source\\repos\\ConsoleApp1\\prac_18\\data.json"))
+            if (!File.Exists("C: \\Users\\belonozhkoin\\Source\\Repos\\C-sharp-sem2\\prac_18\\data.json"))
+                //if (!File.Exists("C:\\Users\\ilyab\\source\\repos\\ConsoleApp1\\prac_18\\data.json"))
             {
-                using (StreamWriter f = new StreamWriter("C:\\Users\\ilyab\\source\\repos\\ConsoleApp1\\prac_18\\data.json"))
+                using (StreamWriter f = new StreamWriter("C:\\Users\\belonozhkoin\\Source\\Repos\\C-sharp-sem2\\prac_18\\data.json"))
+                // using (StreamWriter f = new StreamWriter("C:\\Users\\ilyab\\source\\repos\\ConsoleApp1\\prac_18\\data.json"))
                 {
                 }
             }
 
             // считать и десериализовать
-            using (StreamReader f = new StreamReader("C:\\Users\\ilyab\\source\\repos\\ConsoleApp1\\prac_18\\data.json"))
+            using (StreamReader f = new StreamReader("C:\\Users\\belonozhkoin\\Source\\Repos\\C-sharp-sem2\\prac_18\\data.json"))
+            // using (StreamReader f = new StreamReader("C:\\Users\\ilyab\\source\\repos\\ConsoleApp1\\prac_18\\data.json"))
             {
                 String data = f.ReadToEnd();
                 if (data.Length > 0)
